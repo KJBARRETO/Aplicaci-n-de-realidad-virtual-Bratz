e009f05efcc52e7148c40f8e449e302783cb6b07
Karen Barreto <kjbarreto@uniboyaca.edu.co>
2026-09-24 11:48:03 -0500

    Tutorial

--- Assets/Juego/Scripts/CameraPointerManager.cs
+++ Assets/Juego/Scripts/CameraPointerManager.cs
@@ -93,7 +93,7 @@ public class CameraPointerManager : MonoBehaviour
 
         private void PointerOutGaze()
         {
-            pointer.transform.localScale = Vector3.one * 0.1f;
+            pointer.transform.localScale = Vector3.one * 0.035f;
             pointer.transform.parent.transform.localPosition = new Vector3(0, 0, maxDistancePointer);
             pointer.transform.parent.parent.transform.rotation = transform.rotation;
             GazeManager.Instance.CancelGazeSelection();
